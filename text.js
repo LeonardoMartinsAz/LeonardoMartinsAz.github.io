097d96b73f647ddda2062e9438f4749274a3dcfa
Ana Paula Lourenco <apnl.lourenco@gmail.com>
2025-12-09 08:35:17 -0300

    att

---
+++ text.js
@@ -0,0 +1,14 @@
+const typed = new Typed(".multiple-text", {
+    strings:[
+        "estou estudando programação",
+        "gosto de ler livros",
+        "jogo como hobby",
+        "gosto de fotografar paisagens",
+        
+    ],
+    typeSpeed:60,
+    backSpeed:60,
+    backDelay:100,
+    loop: true,
+  
+});
